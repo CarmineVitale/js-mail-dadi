0f692a8d836614be427ab0a6dab9ea287b356015
CarmineVitale <carmine.vitale90@gmail.com>
2020-03-30 18:22:18 +0200

    Esercizio 2 completato

--- js/main2.js
+++ js/main2.js
@@ -1,1 +1,40 @@
-console.log('questa è la pagina 2');
+// CREAZIONE BOTTONE LANCIA DADO
+var bottoneLancia = document.getElementById('bottoneLancia');
+
+
+bottoneLancia.addEventListener('click',
+    function() {
+        var mioDado = Math.floor(Math.random() * 6 + 1);
+        // console.log(mioDado);
+        var pcDado = Math.floor(Math.random() * 6 + 1);
+        // console.log(pcDado);
+        var result = '';
+        
+        document.getElementById('dado').innerHTML = mioDado
+        document.getElementById('dadopc').innerHTML = pcDado
+        
+        
+        
+        
+        if (mioDado < pcDado) {
+            result = 'HAI PERSO!!!'
+        
+            
+        } else if (mioDado > pcDado) {
+            result = 'HAI VINTO!!!'
+           
+            
+        } else {
+            result = 'PARI!!!'
+            
+            
+        }
+        document.getElementById('risultato').innerHTML = result; 
+
+        var container = document.getElementById('container');
+        console.log(container);
+        
+        container.className = 'show';
+    } )
+
+    
