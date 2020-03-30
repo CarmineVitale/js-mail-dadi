53d4424b5019b93b850aa16d714cb1e4e8abd22e
CarmineVitale <carmine.vitale90@gmail.com>
2020-03-30 16:52:22 +0200

    Esercizio 1 terminato

--- js/main1.js
+++ js/main1.js
@@ -1,1 +1,41 @@
-console.log('ciao pagina 1');
+// Cambio colore titolo lettera per lettera
+
+var titolo = document.getElementById('title');
+
+var titletext = titolo.innerHTML;
+
+var ciao = '';
+
+
+for ( var i = 0; i < titletext.length; i++) {
+    ciao += '<span class=char-' + (i + 1) + '>' + titletext[i] + '</span>';
+    
+}
+titolo.innerHTML = ciao;
+
+
+
+// creazione array
+var list = ['michele@boolean.careers', 'fabio@boolean.careers' , 'roberto@boolean.careers'];
+console.log('Qui è la lista array: ' , list);
+
+// Chiedi all’utente la sua email
+var mail = prompt('Inserisci la tua mail', 'fabio@boolean.careers');
+
+var userFound = false;
+
+for (var i = 0; i < list.length; i++) {
+    var item = list[i];
+
+    if (item == mail) {
+        userFound = true;
+        
+    }
+}
+if (userFound == true) {
+    document.getElementById('accesso').innerHTML = 'MAIL VERIFICATA. ACCESSO CONSENTITO.';
+    
+} else {
+    alert('MAIL NON TROVATA');
+    
+}
